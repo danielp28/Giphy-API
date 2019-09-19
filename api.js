22c249078ca30726d5ca5c4a60fcdad6085f4c06
Daniel Masters <daniel.p.masters@gmail.com>
2019-09-19 17:15:48 -0500

    finished JS and html layout, just needs html styling

--- api.js
+++ api.js
@@ -1,84 +1,91 @@
-(document).ready(function () {
-
-
-
-
-
-
-
-
-
-    $("#submitbtn").on("click", function (event) {
-        event.preventDefault();
-
-        var newBtn = $("<button>");
-        newBtn.addClass("image-button");
-        newBtn.attr("data-image");
-
-        var btnTxt = $("#search").val();
-
-
-
-
-
-        newBtn.text(btnTxt);
-
-        console.log(btnTxt);
-        console.log(newBtn);
-
-        $("#new-buttons").prepend(newBtn);
-
-
-
-
-
-
-
-
-
-
-
-        $(document).on("click", ".image-button", function () {
-
-
-            // var image = $(this).attr("data-image")
-
-            // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            //     image + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
-
-            console.log("this");
-
-            // $.ajax({
-            //     url: queryURL,
-            //     method: "GET"
-            // })
-
-            //     .then(function (response) {
-            //         var results = response.data;
-            //         console.log(results);
-
-            //         for (var i = 0; i < results.length; i++) {
-            //             if (results[i].rating !== "g" && results[i].rating !== "pg-13") {
-
-
-            //                 var imageDiv = $("<div>");
-            //                 var newImg = $("<img>");
-            //                 newImg.attr("src", results[i].images.fixed_height.url);
-            //                 var rating = results[i].rating;
-            //                 var p = $("<p>").text("Rating: " + rating);
-
-
-
-            //                 imageDiv.append(p);
-            //                 imageDiv.append(newImg);
-            //                 $("#gifs-here").prepend(imageDiv);
-
-
-            //             }
-            //         }
-            //     })
+$( document ).ready(function() {
+    // An array of actions, new actions will be pushed into this array;
+    var actions = ["Ollie", "Kickflip", "Treflip", "Nollie", "Bigspin", "Double Flip", "Benihina", "Manual", "Nollie Flip", "180"];
+    // Creating Functions & Methods
+    // Function that displays all gif buttons
+    function displayGifButtons(){
+        $("#gifButtonsView").empty(); // erasing anything in this div id so that it doesnt duplicate the results
+        for (var i = 0; i < actions.length; i++){
+            var gifButton = $("<button>");
+            gifButton.addClass("action");
+            gifButton.addClass("btn btn-primary")
+            gifButton.attr("data-name", actions[i]);
+            gifButton.text(actions[i]);
+            $("#gifButtonsView").append(gifButton);
+        }
+    }
+    // Function to add a new action button
+    function addNewButton(){
+        $("#addGif").on("click", function(){
+        var action = $("#action-input").val().trim();
+        if (action == ""){
+          return false; // added so user cannot add a blank button
+        }
+        actions.push(action);
+    
+        displayGifButtons();
+        return false;
+        });
+    }
+    // Function to remove last action button
+        // Doesnt work properly yet removes all of the added buttons
+        // rather than just the last
+    function removeLastButton(){
+        $("removeGif").on("click", function(){
+        actions.pop(action);
+        displayGifButtons();
+        return false;
+        });
+    }
+    // Function that displays all of the gifs
+    function displayGifs(){
+        var action = $(this).attr("data-name");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=dc6zaTOxFJmzC&limit=10";
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
         })
-    })
-
-
-})
+        .done(function(response) {
+            $("#gifsView").empty(); // erasing anything in this div id so that it doesnt keep any from the previous click
+            var results = response.data; //shows results of gifs
+            if (results == ""){
+              alert("There isn't a gif for this selected button");
+            }
+            for (var i=0; i<results.length; i++){
+    
+                var gifDiv = $("<div>"); //div for the gifs to go inside
+                gifDiv.addClass("gifDiv");
+                // pulling rating of gif
+                var gifRating = $("<p>").text("Rating: " + results[i].rating);
+                gifDiv.append(gifRating);
+                // pulling gif
+                var gifImage = $("<img>");
+                gifImage.attr("src", results[i].images.fixed_height_small_still.url); // still image stored into src of image
+                gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); // still image
+                gifImage.attr("data-animate",results[i].images.fixed_height_small.url); // animated image
+                gifImage.attr("data-state", "still"); // set the image state
+                gifImage.addClass("image");
+                gifDiv.append(gifImage);
+                // pulling still image of gif
+                // adding div of gifs to gifsView div
+                $("#gifsView").prepend(gifDiv);
+            }
+        });
+    }
+    // Calling Functions & Methods
+    displayGifButtons(); // displays list of actions already created
+    addNewButton();
+    removeLastButton();
+    // Document Event Listeners
+    $(document).on("click", ".action", displayGifs);
+    $(document).on("click", ".image", function(){
+        var state = $(this).attr('data-state');
+        if ( state == 'still'){
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        }else{
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+    });
+    });
